7cc01a894fc269581e76eb750f82ca9e2a288e68
K B Tanvir <tanvirkhaan004@gmail.com>
2022-08-12 22:41:26 +0600

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import App from "./Routes";
 import store from "./lib/store/store";
 
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
